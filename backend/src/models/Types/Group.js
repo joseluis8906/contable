4bd491718bdc98bfdbe7fc24eed611c9cda2503e
Jose Cáceres <joseluis8906@opmbx.org>
2017-11-09 17:02:28 -0500

    actualizado

---
+++ backend/src/models/Types/Group.js
@@ -0,0 +1,84 @@
+import { GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList } from 'graphql';
+
+import Db from '../Db.js';
+import {User} from './User.js';
+
+const Group = new GraphQLObjectType({
+  name: "Group",
+  description: "Object representation of Group",
+  fields: () => {
+    return {
+      Id: {
+        type: GraphQLInt,
+        resolve(Group) {
+          return Group.Id;
+        }
+      },
+      Name: {
+        type: GraphQLString,
+        resolve(Group) {
+          return Group.Name;
+        }
+      },
+      Users: {
+        type: new GraphQLList(User),
+        resolve(Group) {
+          return Group.getUsers();
+        }
+      }
+    };
+  }
+});
+
+const Groups = {
+  type: new GraphQLList(Group),
+  args: {
+    Id: {type: GraphQLInt},
+    Name: {type: GraphQLString}
+  },
+  resolve(root, args) {
+    return Db.models.Group.findAll({where: args});
+  }
+};
+
+const CreateGroup = {
+  type: Group,
+  args: {
+    Name: {type: GraphQLString},
+  },
+  resolve(_, args) {
+    return Db.models.Group.create({
+      Name: args.Name
+    });
+  }
+};
+
+
+const UpdateGroup = {
+  type: Group,
+  args: {
+    Id: {type: GraphQLInt},
+    Name: {type: GraphQLString},
+  },
+  resolve(_, args) {
+    return Db.models.Group.findOne({
+      where: {Id: args.Id}
+    }).then (R => {
+      R.Name = args.Name;
+      R.save();
+      return R;
+    });
+  }
+};
+
+
+export {
+  Group,
+  Groups,
+  CreateGroup,
+  UpdateGroup
+}
---
+++ backend/src/models/Types/User.js
@@ -0,0 +1,165 @@
+import { GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList } from 'graphql';
+
+import Db from '../Db.js';
+import {Group} from './Group.js';
+
+const User = new GraphQLObjectType({
+  name: "User",
+  description: "Object representation of User",
+  fields: () => {
+    return {
+      Id: {
+        type: GraphQLInt,
+        resolve(User) {
+          return User.Id;
+        }
+      },
+      UserName: {
+        type: GraphQLString,
+        resolve(User) {
+          return User.UserName;
+        }
+      },
+      Password: {
+        type: GraphQLString,
+        resolve(User) {
+          return User.Password;
+        }
+      },
+      Active: {
+        type: GraphQLString,
+        resolve(User) {
+          return User.Active;
+        }
+      },
+      Groups: {
+        type: new GraphQLList(Group),
+        resolve(User) {
+          return User.getGroups();
+        }
+      }
+    };
+  }
+});
+
+const Users = {
+  type: new GraphQLList(User),
+  args: {
+    Id: {type: GraphQLInt},
+    UserName: {type: GraphQLString},
+    Password: {type: GraphQLString},
+    Active: {type: GraphQLString}
+  },
+  resolve(root, args) {
+    return Db.models.User.findAll({where: args});
+  }
+};
+
+
+const CreateUser = {
+  type: User,
+  args: {
+    UserName: {type: GraphQLString},
+    Password: {type: GraphQLString},
+    Active: {type: GraphQLString}
+  },
+  resolve(_, args) {
+    return Db.models.User.create({
+      UserName: args.UserName,
+      Password: args.Password,
+      Active: args.Active
+    });
+  }
+};
+
+
+const UpdateUser = {
+  type: User,
+  args: {
+    Id: {type: GraphQLInt},
+    UserName: {type: GraphQLString},
+    Password: {type: GraphQLString},
+    Active: {type: GraphQLString}
+  },
+  resolve(_, args) {
+    return Db.models.User.findOne({
+      where: {Id: args.Id}
+    }).then( R => {
+      R.Password = args.Password
+      R.Active = args.Active
+      R.save()
+      return R;
+    });
+  }
+};
+
+
+const UserAddGroup = {
+  type: User,
+  args: {
+    UserId: {type: GraphQLInt},
+    GroupId: {type: GraphQLInt}
+  },
+  resolve(_, args) {
+    return Db.models.User.findOne({
+      where: {Id: args.UserId}
+    }).then(U => {
+      if (U !== null){
+        return Db.models.Group.findOne({
+          where: {Id: args.GroupId}
+        }).then(G => {
+          if (G !== null) {
+            return U.addGroup(G).then(R => {
+              return U;
+            })
+          } else {
+            return U;
+          }
+        })
+      } else {
+        return U;
+      }
+    });
+  }
+};
+
+
+const UserRemoveGroup = {
+  type: User,
+  args: {
+    UserId: {type: GraphQLInt},
+    GroupId: {type: GraphQLInt},
+  },
+  resolve(_, args) {
+    return Db.models.User.findOne({
+      where: {Id: args.UserId}
+    }).then(U => {
+      if (U !== null){
+        return Db.models.Group.findOne({
+          where: {Id: args.GroupId}
+        }).then(G => {
+          if (G !== null) {
+            U.removeGroup(G)
+          }
+          return U;
+        })
+      } else {
+        return U;
+      }
+    });
+  }
+};
+
+
+export {
+  User,
+  Users,
+  CreateUser,
+  UpdateUser,
+  UserAddGroup,
+  UserRemoveGroup
+}
