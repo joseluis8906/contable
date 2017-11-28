6897fee547993b85347393af954b067c0a93ad92
Jose Cáceres <joseluis8906@opmbx.org>
2017-11-28 14:48:21 -0500

    actualizado

--- frontend/pages/ingreso.vue
+++ frontend/pages/ingreso.vue
@@ -26,7 +26,7 @@ v-layout( align-center justify-center )
         v-layout( row wrap)
           v-flex( xs12 )
 
-            v-text-field( label="Número" )
+            v-text-field( label="Número" v-model="Ingreso.Numero")
 
 
             v-menu( lazy
@@ -40,14 +40,14 @@ v-layout( align-center justify-center )
 
               v-text-field( slot="activator"
                             label="Fecha"
-                            v-model="Causacion.Fecha"
+                            v-model="Ingreso.Fecha"
                             readonly )
 
               v-date-picker( :months="months"
                            :days="days"
                            first-day-of-week="D"
                            :header-date-format="({ monthName, year }) => { return `${monthName} ${year}` }"
-                           v-model="Causacion.Fecha"
+                           v-model="Ingreso.Fecha"
                            dark )
                 template( scope="{ save, cancel }" )
                   v-card-actions
@@ -55,11 +55,11 @@ v-layout( align-center justify-center )
 
 
             v-text-field( label="Concepto"
-                          v-model="Causacion.Concepto"
+                          v-model="Ingreso.Concepto"
                           multi-line )
 
             v-data-table( :headers="Tabla.Headers"
-                          :items="Causacion.Items"
+                          :items="Ingreso.Items"
                           class="elevation-5 grey lighten-1 grey--text text--darken-4" )
 
               template(slot="headers" scope="props")
@@ -78,7 +78,7 @@ v-layout( align-center justify-center )
                          small
                          error
                          style="width: 16px; height:16px"
-                         @click.native="eliminar(props.item)")
+                         @click.native="Eliminar(props.item)")
                     v-icon(dark) remove
 
             v-select( label="Tipo"
@@ -91,12 +91,13 @@ v-layout( align-center justify-center )
 
             v-money(label="Monto" v-model="Monto" maskType="currency")
 
-            v-btn(fab dark class="indigo mt-0" @click.native="agregar")
+            v-btn(fab dark class="blue mt-0" @click.native="Agregar"  :disabled="Ingreso.Id === null")
               v-icon(dark) add
 
       v-card-actions
         v-spacer
         v-btn( dark warning @click.native="Reset" ) Limpiar
+        v-btn( dark primary @click.native="Guardar" ) Guardar
 </template>
 
 <script>
@@ -105,7 +106,14 @@ import TERCEROS from '~/queries/Terceros.gql';
 import CREATE_TERCERO from '~/queries/CreateTercero.gql';
 import UPDATE_TERCERO from '~/queries/UpdateTercero.gql';
 
+import PERIODOS from '~/queries/Periodos.gql';
+
 import CONFINGRESOS from '~/queries/ConfIngresos.gql';
+
+import INGRESOS from '~/queries/Ingresos.gql';
+import CREATE_INGRESO from '~/queries/CreateIngreso.gql';
+import INGRESO_ADD_ITEM from '~/queries/IngresoAddItem.gql';
+
 
 import VMoney from '~/components/MonetaryInput.vue';
 
@@ -132,11 +140,12 @@ export default {
       'Noviembre',
       'Diciembre'],
     days: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
-    Causacion: {
+    Ingreso: {
       Id: null,
       Numero: null,
       Fecha: null,
       Concepto: null,
+      Total: null,
       Items: [],
     },
     ConfIngresos: [],
@@ -166,258 +175,39 @@ export default {
         this.ConfIngresos = data.ConfIngresos;
       }
     },
-  },
-  methods: {
-    agregar () {
-      let tmp = Object.assign({Monto: this.Monto}, this.ConfIngreso);
-      this.Causacion.Items.push(tmp);
-    },
-    eliminar(){
-      this.ItemsCausacion = [];
-    },
-    CreateOrUpdate () {
-      if (this.Id === null) {
-        this.Create();
-      }else{
-        this.Update();
-      }
-    },
-    Create () {
-      const Tercero = {
-        TipoDeIdentificacion: this.TipoDeIdentificacion,
-        NumeroDeIdentificacion: this.NumeroDeIdentificacion,
-        DigitoDeVerificacion: this.DigitoDeVerificacion,
-        PrimerApellido: this.PrimerApellido,
-        SegundoApellido: this.SegundoApellido,
-        PrimerNombre: this.PrimerNombre,
-        OtrosNombres: this.OtrosNombres,
-        RazonSocial: this.RazonSocial,
-        Direccion: this.Direccion,
-        CodigoDepartamento: this.CodigoDepartamento,
-        CodigoMunicipio: this.CodigoMunicipio,
-        PaisDeResidencia: this.PaisDeResidencia
-      };
-
-      this.Reset ();
-
-      this.$apollo.mutate ({
-        mutation: CREATE_TERCERO,
-        variables: {
-          TipoDeIdentificacion: Tercero.TipoDeIdentificacion,
-          NumeroDeIdentificacion: Tercero.NumeroDeIdentificacion,
-          DigitoDeVerificacion: Tercero.DigitoDeVerificacion,
-          PrimerApellido: Tercero.PrimerApellido,
-          SegundoApellido: Tercero.SegundoApellido,
-          PrimerNombre: Tercero.PrimerNombre,
-          OtrosNombres: Tercero.OtrosNombres,
-          RazonSocial: Tercero.RazonSocial,
-          Direccion: Tercero.Direccion,
-          CodigoDepartamento: Tercero.CodigoDepartamento,
-          CodigoMunicipio: Tercero.CodigoMunicipio,
-          PaisDeResidencia: Tercero.PaisDeResidencia
-      },
-      loadingKey: 'loading',
-      update: (store, { data: res }) => {
-        //console.log(Ente);
-        try{
-          var data = store.readQuery({
-            query: TERCEROS,
-            variables: {
-              TipoDeIdentificacion: res.CreteTercero.TipoDeIdentificacion,
-              NumeroDeIdentificacion: res.CreateTercero.NumeroDeIdentificacion,
-              DigitoDeVerificacion: res.CreateTercero.DigitoDeVerificacion
-            }
-          })
-
-          console.log(data)
-          data.Terceros.push(res.CreateTercero)
-
-          store.writeQuery({
-            query: TERCEROS,
-            variables: {
-              TipoDeIdentificacion: res.CreateTercero.TipoDeIdentificacion,
-              NumeroDeIdentificacion: res.CreateTercero.NumeroDeIdentificacion,
-              DigitoDeVerificacion: res.CreateTercero.DigitoDeVerificacion
-            },
-            data: data
-          })
-
-        } catch (Err) {
-
-          var data = {Terceros: []}
-
-          data.Terceros.push(res.CreateTercero)
-
-          store.writeQuery({
-            query: TERCEROS,
-            variables: {
-              TipoDeIdentificacion: res.CreateTercero.TipoDeIdentificacion,
-              NumeroDeIdentificacion: res.CreateTercero.NumeroDeIdentificacion,
-              DigitoDeVerificacion: res.CreateTercero.DigitoDeVerificacion
-            },
-            data: data
-          })
-
+    QIngresos: {
+      query: INGRESOS,
+      variables () {
+        return {
+          Numero: this.Ingreso.Numero,
         }
-
       },
-      }).then( data => {
-        //console.log(data)
-      }).catch( Err => {
-        //console.log(Err)
-      })
-    },
-    Update () {
-      const Tercero = {
-        Id: this.Id,
-        TipoDeIdentificacion: this.TipoDeIdentificacion,
-        NumeroDeIdentificacion: this.NumeroDeIdentificacion,
-        DigitoDeVerificacion: this.DigitoDeVerificacion,
-        PrimerApellido: this.PrimerApellido,
-        SegundoApellido: this.SegundoApellido,
-        PrimerNombre: this.PrimerNombre,
-        OtrosNombres: this.OtrosNombres,
-        RazonSocial: this.RazonSocial,
-        Direccion: this.Direccion,
-        CodigoDepartamento: this.CodigoDepartamento,
-        CodigoMunicipio: this.CodigoMunicipio,
-        PaisDeResidencia: this.PaisDeResidencia
-      };
-
-      this.Reset ();
-
-      this.$apollo.mutate ({
-        mutation: UPDATE_TERCERO,
-        variables: {
-          Id: Tercero.Id,
-          TipoDeIdentificacion: Tercero.TipoDeIdentificacion,
-          NumeroDeIdentificacion: Tercero.NumeroDeIdentificacion,
-          DigitoDeVerificacion: Tercero.DigitoDeVerificacion,
-          PrimerApellido: Tercero.PrimerApellido,
-          SegundoApellido: Tercero.SegundoApellido,
-          PrimerNombre: Tercero.PrimerNombre,
-          OtrosNombres: Tercero.OtrosNombres,
-          RazonSocial: Tercero.RazonSocial,
-          Direccion: Tercero.Direccion,
-          CodigoDepartamento: Tercero.CodigoDepartamento,
-          CodigoMunicipio: Tercero.CodigoMunicipio,
-          PaisDeResidencia: Tercero.PaisDeResidencia
-        },
-        loadingKey: 'loading',
-        update: (store, { data: res }) => {
-          //console.log(Ente);
-          try {
-            var data = store.readQuery({
-              query: TERCEROS,
-              variables: {
-                TipoDeIdentificacion: res.UpdateTercero.TipoDeIdentificacion,
-                NumeroDeIdentificacion: res.UpdateTercero.NumeroDeIdentificacion,
-                DigitoDeVerificacion: res.UpdateTercero.DigitoDeVerificacion
-              }
-            })
-
-            for (let i=0; i<data.Terceros.length; i++) {
-              if (data.Terceros[i].Id === res.UpdateTercero.Id) {
-                data.Terceros[i].TipoDeIdentificacion = res.UpdateTercero.TipoDeIdentificacion
-                data.Terceros[i].NumeroDeIdentificacion = res.UpdateTercero.NumeroDeIdentificacion
-                data.Terceros[i].DigitoDeVerificacion = res.UpdateTercero.DigitoDeVerificacion
-                data.Terceros[i].PrimerApellido = res.UpdateTercero.PrimerApellido
-                data.Terceros[i].SegundoApellido = res.UpdateTercero.SegundoApellido
-                data.Terceros[i].PrimerNombre = res.UpdateTercero.PrimerNombre
-                data.Terceros[i].OtrosNombres = res.UpdateTercero.OtrosNombres
-                data.Terceros[i].RazonSocial = res.UpdateTercero.RazonSocial
-                data.Terceros[i].Direccion = res.UpdateTercero.Direccion
-                data.Terceros[i].CodigoDepartamento = res.UpdateTercero.CodigoDepartamento
-                data.Terceros[i].CodigoMunicipio = res.UpdateTercero.CodigoMunicipio
-                data.Terceros[i].PaisDeResidencia = res.UpdateTercero.PaisDeResidencia
-              }
-            }
-
-            store.writeQuery({
-              query: TERCEROS,
-              variables: {
-                TipoDeIdentificacion: res.UpdateTercero.TipoDeIdentificacion,
-                NumeroDeIdentificacion: res.UpdateTercero.NumeroDeIdentificacion,
-                DigitoDeVerificacion: res.UpdateTercero.DigitoDeVerificacion
-              },
-              data: data
-            })
-
-          } catch (Err) {
-
-            var data = {Terceros: []}
-
-            data.Terceros.push(res.UpdateTercero)
-
-            store.writeQuery({
-              query: TERCEROS,
-              variables: {
-                TipoDeIdentificacion: res.UpdateTercero.TipoDeIdentificacion,
-                NumeroDeIdentificacion: res.UpdateTercero.NumeroDeIdentificacion,
-                DigitoDeVerificacion: res.UpdateTercero.DigitoDeVerificacion
-              },
-              data: data
-            })
-
-          }
-
-        },
-      }).then( data => {
-        //console.log(data)
-      }).catch( Err => {
-        //console.log(Err)
-      })
-    },
-    Reset () {
-      this.Id = null
-      this.TipoDeIdentificacion = null
-      this.NumeroDeIdentificacion = null
-      this.DigitoDeVerificacion = null
-      this.PrimerApellido = null
-      this.SegundoApellido = null
-      this.PrimerNombre = null
-      this.OtrosNombres = null
-      this.RazonSocial = null
-      this.Direccion = null
-      this.CodigoDepartamento = null
-      this.CodigoMunicipio = null
-      this.PaisDeResidencia = null
-    },
-    LoadTercero (Terceros) {
-      //console.log (Terceros)
-      for (let i=0; i<Terceros.length; i++) {
-        if (
-          this.TipoDeIdentificacion === Terceros[i].TipoDeIdentificacion
-          &&
-          this.NumeroDeIdentificacion === Terceros[i].NumeroDeIdentificacion
-          &&
-          this.DigitoDeVerificacion === Terceros[i].DigitoDeVerificacion
-        ) {
-          this.Id = Terceros[i].Id
-          this.PrimerApellido = Terceros[i].PrimerApellido
-          this.SegundoApellido = Terceros[i].SegundoApellido
-          this.PrimerNombre = Terceros[i].PrimerNombre
-          this.OtrosNombres = Terceros[i].OtrosNombres
-          this.RazonSocial = Terceros[i].RazonSocial
-          this.Direccion = Terceros[i].Direccion
-          this.CodigoDepartamento = Terceros[i].CodigoDepartamento
-          this.CodigoMunicipio = Terceros[i].CodigoMunicipio
-          this.PaisDeResidencia = Terceros[i].PaisDeResidencia
-          break
-        }else{
-          this.Id = null
-          this.PrimerApellido = null
-          this.SegundoApellido = null
-          this.PrimerNombre = null
-          this.OtrosNombres = null
-          this.RazonSocial = null
-          this.Direccion = null
-          this.CodigoDepartamento = null
-          this.CodigoMunicipio = null
-          this.PaisDeResidencia = null
+      update (data) {
+        if(data.Ingresos.length > 0){
+          this.Ingreso = data.Ingresos[0]
         }
+        console.log(this.Ingreso)
       }
     }
+  },
+  methods: {
+    Agregar () {
+      let tmp = Object.assign({Monto: this.Monto}, this.ConfIngreso);
+      this.Ingreso.Items.push(tmp);
+    },
+    Eliminar(){
+
+    },
+    Reset () {
+      this.Ingreso = {
+        Id: null,
+        Numero: null,
+        Fecha: null,
+        Concepto: null,
+        Total: null,
+        Items: [],
+      };
+    },
   },
   components: {
     VMoney
